b92adfa9b1b72182036124c640490c03f0bcf6e7
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-04-16 11:07:43 +0200

    Small fixes to mojo

--- qualityguardian-maven-plugin/src/main/java/com/github/dannil/qualityguardian/mavenplugin/mojo/TestMojo.java
+++ qualityguardian-maven-plugin/src/main/java/com/github/dannil/qualityguardian/mavenplugin/mojo/TestMojo.java
@@ -10,7 +10,7 @@ import org.apache.maven.project.MavenProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mojo(name = "test", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+@Mojo(name = "test", defaultPhase = LifecyclePhase.TEST)
 public class TestMojo extends AbstractMojo {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestMojo.class);
@@ -20,38 +20,6 @@ public class TestMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException {
-        
+        LOGGER.info(String.format("Executing tests on artifact %s", project.getArtifactId()));
     }
-
-    // @Parameter(defaultValue = "${project.build.directory}", property = "outputDir",
-    // required = true)
-    // private File outputDirectory;
-
-    // @Override
-    // public void execute() throws MojoExecutionException {
-    // File f = outputDirectory;
-    //
-    // if (!f.exists()) {
-    // f.mkdirs();
-    // }
-    //
-    // File touch = new File(f, "touch.txt");
-    //
-    // FileWriter w = null;
-    // try {
-    // w = new FileWriter(touch);
-    //
-    // w.write("touch.txt");
-    // } catch (IOException e) {
-    // throw new MojoExecutionException("Error creating file " + touch, e);
-    // } finally {
-    // if (w != null) {
-    // try {
-    // w.close();
-    // } catch (IOException e) {
-    // // ignore
-    // }
-    // }
-    // }
-    // }
 }
